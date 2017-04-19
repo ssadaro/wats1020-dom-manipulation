4198d1eaaf91e0c6135793aafc8e1569fff76bf0
s.a.sadaro <s.a.sadaro@gmail.com>
2017-04-19 17:21:18 -0400

    final changes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-//////////////////////////////////////////////////
-// WATS1020 Dom Manipulation
-// Custom script goes here.
-//////////////////////////////////////////////////
 
 $( document ).ready(function() {
     var userInfo = {
@@ -22,8 +18,13 @@ $( document ).ready(function() {
     //      2. Fill the user's first and last name into `div.user-info`.
     //      (NOTE: You do not have to perform any validation on the data as
     //          a base requirement.)
-
-
+  
+    $('#login-form .btn').on('click', function(event) {
+    $('#login-form').hide();
+    $('.user-info').show();
+    $('.user-fullname').text(' ' + userInfo.firstName + ' ' + userInfo.lastName);
+  
+});
     // TODO: Create a function to listen for clicks on all the "View Details"
     // buttons so that when a user clicks a "View Details" button they see
     // the content contained in the elements with the class "details" in the
@@ -33,6 +34,21 @@ $( document ).ready(function() {
     //      3. Toggle visibility of all the elements within that parent with the class `details`.
     //      4. Change the text of the "view details" button to read "hide details" so the user
     //          understands they can hide the text again.
+      $('.view-details').on('click', function(event){                 
+        console.log(event);
+          var targetElement = event.target;                         
+          var container = targetElement.parentElement.parentElement;
+          $(container).find('.details').each(function(index, el){   
+              if ($(el).is(':visible')){                            
+                  $(el).fadeOut();                                  
+                  targetElement.innerText = "View Details"          
+
+              } else {                                             
+                  $(el).fadeIn();
+                  targetElement.innerText = "Hide Details"
+              }
+          });
+    });
 
     // TODO: Create a function that listens for clicks on the voting buttons and
     // looks at the `data-vote` attribute on each button to see what was voted for,
@@ -43,5 +59,22 @@ $( document ).ready(function() {
     //      3. Increment the counter for whichever vote talley is affected.
     //      4. Determine the respective percentages (out of 100) for each progress bar.
     //      5. Modify the `width` attribute on each progress bar to set the updated percentage.
-
+    $('.vote').on('click', function(event){                                   /*1*/
+        console.log(event);
+        if ($(this).attr('data-vote')==="great"){                             /*2*/
+            voteCounts.great++;                                               /*3*/
+            voteCounts.total++;
+        }else{
+            if ($(this).attr('data-vote')==="greatest"){                      
+                voteCounts.greatest++;                                        
+                voteCounts.total++;
+            }
+        }
+    var greatPercent = voteCounts.great / voteCounts.total * 100 + '%';      /*4*/
+    var greatestPercent = voteCounts.greatest / voteCounts.total * 100 + '%'; 
+    $('.great-progress').css('width', greatPercent);                          /*5*/
+    $('.greatest-progress').css('width', greatestPercent);  
+    });
 });
+
+
